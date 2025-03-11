2d906ccbf91ccc9803925ca45d661f225ba73298
abdul <abdulmahmed9@gmail.com>
2025-03-11 14:47:48 +0530

    Added program to check for intersection in two arrays

--- array_string.py
+++ array_string.py
@@ -11,3 +11,15 @@ for index,i in enumerate(arr):
 
     dict1[i] = index
 
+
+a1 = [4, 2, 1, 6]
+a2 = [3, 6, 9, 2, 10]
+res = []
+
+s = set(a1)
+
+for i in a2:
+    if i in s:
+        res.append(i)
+
+print(res)
